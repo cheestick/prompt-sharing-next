17dd3eed29189da560da7c7cc573e57dc4e1d831
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-09 22:26:20 +0300

    connect to db feature was implemented

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+let isConnected = false // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true)
+
+  if (isConnected) {
+    console.log('MongoDB is alredy connected')
+    return
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_promprt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    isConnected = true
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error.message)
+    isConnected = false
+  }
+}
