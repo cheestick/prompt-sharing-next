0ddeebecb21449e382271615fa8a4ea945b29970
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-10 10:36:43 +0300

    next auth was fixed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
-import { signIn } from 'next-auth/react'
+import User from '@models/user'
 import { connectToDB } from '@utils/database'
 
 const handler = NextAuth({
@@ -10,20 +10,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      // serverless -> lambda -> dynamodb
-      await connectToDB()
-      // check if a user already exists
 
-      // if not, create a new user
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session?.user?.email,
+      })
+      session.user.id = sessionUser?._id.toString()
+      return session
+    },
 
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
-    }
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda -> dynamodb
+        await connectToDB()
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email })
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile?.email,
+            username: profile?.name.replace(' ', '').toLowerCase(), // no spaces
+            image: profile?.picture,
+          })
+        }
+
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    },
   },
 })
 
